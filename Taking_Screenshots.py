4553436ea808605dcc7c065561804e5c8dcb13ec
pritesh460 <kalsariyapritesh9999@gmail.com>
2026-02-20 18:43:45 +0530

    Taking screenshots

--- Taking_Screenshots.py
+++ Taking_Screenshots.py
@@ -1,5 +1,13 @@
-import pyscreenshot
-image = pyscreenshot.grab(bbox=(10, 10, 500, 500))
-# image = pyscreenshot.grab()
-image.show()
-image.save("screenshot.png")
+import pyautogui
+import random
+import time
+
+while True:
+    random_time = random.randint(1, 5)
+
+    time.sleep(random_time)
+
+    myScreenshot = pyautogui.screenshot()
+
+    file_name = str(time.time())+".png"
+    myScreenshot.save(file_name)
